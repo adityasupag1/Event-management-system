5a464d5636091282f5a1649b15b2b37b54c6e32a
Aditya Raj <araj15397@gmail.com>
2026-04-18 22:58:20 +0530

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://event-management-system-cui.netlify.app'
+    ],
     credentials: true,
   })
 );
